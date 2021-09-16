e3aa46ab695ae8b4572d393da5898b2a985875a7
Buck Tower <bucktower@utexas.edu>
2021-09-15 22:07:21 -0700

    Optimize images

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,15 +1,27 @@
+import Image from "next/image";
+
 export function BackgroundImage({ image, movedUp }: any) {
+  console.log(image.fields.file.url);
   return (
     <div
       style={{
         width: "100%",
         height: "100vh",
-        backgroundImage: `url(${image.fields.file.url})`,
+        // backgroundImage: `url(${image.fields.file.url})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         transition: "all .2s ease-in-out",
         transform: movedUp && "translateY(-75%) scale(0.8)",
+        zIndex: -10000,
+        position: "relative",
       }}
-    />
+    >
+      <Image
+        layout="fill"
+        className="object-center object-cover"
+        src={`https:${image.fields.file.url}`}
+        // alt={title}
+      />
+    </div>
   );
 }
